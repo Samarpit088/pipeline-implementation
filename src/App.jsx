890602dd6ee96e3af74116991a750e0827f69b1f
samarpitSharma88 <samarpit_sharma@icloud.com>
2025-05-01 20:40:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>My react app </h1>
+      <p> this is a simple react app with ci/cd pipelines</p>
+      <h1> with GITHUB ACTION</h1>
+    </>
+  )
+}
+
+export default App
